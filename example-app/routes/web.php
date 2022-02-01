9d8d927e497a0f379bf196935e7080e0c766d0ea
Black <53127242+Aubert-co@users.noreply.github.com>
2022-01-31 22:59:56 -0300

    Update web.php

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Persons;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[Persons::class,'index']);
+
+
+Route::get('/find/{name}/{values}',[Persons::class,'find'])->where(['name'=>'pet','id','name']);
+
+
+Route::delete('/persons/{id}',[Persons::class,'destroy']);
+
