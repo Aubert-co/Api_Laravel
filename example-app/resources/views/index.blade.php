77b0cd7341cd8cf12c5115a30b1c1d36f9a46744
Black <53127242+Aubert-co@users.noreply.github.com>
2022-02-01 18:19:22 -0300

    Update index.blade.php

--- example-app/resources/views/index.blade.php
+++ example-app/resources/views/index.blade.php
@@ -10,6 +10,13 @@
     <main>
         <div class="flex">
             @csrf
+
+            <div class="search_persons">
+                <select name="" id="">
+
+                </select>
+                <input type="text">
+            </div>
             <table>
                 <thead>
                     <tr>
@@ -38,9 +45,28 @@
 
                 </tbody>
             </table>
+            <div class="create">
+                <input type="text">
+                <input type="text">
+                <input type="text">
+                <button>confirmar</button>
+                <button>cancelar</button>
+            </div>
             <div class="itens_selectAll">
-                <input type="checkbox" id="check_allCheckbox" onclick="check_all(this.checked)">marcar todos
-            apagar selecionados editar selecionados
+                <div class="markselect">
+                    <input type="checkbox" id="check_allCheckbox" onclick="check_all(this.checked)">
+                    <div class="checkAllDiv">
+                        marcar todos
+                    </div>
+                </div>
+
+                <div class="deleteSelect">
+                    <button onclick="Delete_All()">Apagar Selecionados</button>
+                </div>
+                <div class="editSelect" onclick="Edit_All()">
+                    <button>Editar Selecionados</button>
+                </div>
+
             </div>
         </div>
     </main>
@@ -48,9 +74,10 @@
     <script >
 
 
-
-        var check_allCheckbox =$("#check_allCheckbox")
+        //Obeservação usei o selector do jquery mas bugou então usei os que o js fornece mesclado com jquery
+        var check_allCheckbox =document.querySelector("#check_allCheckbox")
         var checkInputs = document.querySelectorAll('#check')
+        var checkAllDiv = $('.checkAllDiv')
         var values = new Map()
         var checkbox_lenght = checkInputs.length
         var selecionado =false
@@ -62,17 +89,25 @@
 
             if(checked){
                 values.set(`CheckBoxValue=${id}`,id)
+                checkAllDiv.html(`desmarcar todos total:${values.size}`)
+                check_allCheckbox.checked = true
             }else{
                 values.delete(`CheckBoxValue=${id}`)
+                checkAllDiv.html(`desmarcar todos total:${values.size}`)
             }
             if(values.sizes > 1)selecionado = true
             if(values.size < checkbox_lenght +1){
+
+
+            }
+            if(values.size === 0 ){
+                checkAllDiv.html(`marcar todos total:${values.size}`)
                 check_allCheckbox.checked = false
             }
             if(values.size  === checkbox_lenght ){
                 check_allCheckbox.checked = true
+                checkAllDiv.html(`desmarcar todos total:${values.size}`)
             }
-            console.log(values.size,selecionado)
         }
         /**
          *
@@ -80,45 +115,57 @@
 
 
         function check_all(checked){
-    console.log('here')
-    if(checked){
 
-        checkInputs.forEach((val)=>{
-            val.checked = true
-            const ids = val.value
-            values.set(`CheckBoxValue=${ids}`,ids)
-        })
-       return selecionado = true
-    }
-    checkInputs.forEach((val)=>{
-            val.checked = false
-        })
-        values.clear()
-        selecionado = false
-}
+            if(checked){
+                checkInputs.forEach((val)=>{
+                    val.checked = true
+                    const ids = val.value
+                    values.set(`CheckBoxValue=${ids}`,ids)
+                })
+                checkAllDiv.html(`desmarcar todos total:${values.size}`)
+                return selecionado = true
+            }
+                checkInputs.forEach((val)=>{
+                    val.checked = false
+                })
+                values.clear()
+                selecionado = false
+                checkAllDiv.html(`marcar todos total:${values.size}`)
+        }
 
         function Delete(id){
+
+            var token = document.querySelector('input[name=_token]').value
+
+            $.ajax({
+                method:"DELETE",
+                url:`http://localhost:8000/deleteone/${id}`,
+                headers:{'X-CSRF-TOKEN':token},
+                success:function(data){
+                $('body').html( data )
+                }
+            })
+        }
+        function Delete_All(){
             const data = values.values()
             const ids = [...data]
 
-            const div = document.querySelector('.flex')
-            const token = document.querySelector('input[name=_token]').value
-             fetch(`http://localhost:8000/persons/${ids}`,{
-                method:'DELETE',
-                headers:{
-                    'X-CSRF-TOKEN':token
+            var div = document.querySelector('.flex')
+            var token = document.querySelector('input[name=_token]').value
+
+            $.ajax({
+                method:"DELETE",
+                url:`http://localhost:8000/deletemany`,
+                headers:{'X-CSRF-TOKEN':token},
+                data:{ids},
+                success:function(data){
+                $('body').html( data )
                 }
             })
-            .then((data)=>data.text())
-                .then((datas)=>{
-
-                })
-
-                .catch((err)=>{
-                    console.log('err',err)
-                })
         }
+        function Edit_All(){
 
+        }
     </script>
 </body>
 </html>
