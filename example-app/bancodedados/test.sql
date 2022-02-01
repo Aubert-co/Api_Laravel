d89349795030682e4eef6fbf429fbff2f98fdd3c
Black <53127242+Aubert-co@users.noreply.github.com>
2022-02-01 18:19:37 -0300

    Create test.sql

---
+++ example-app/bancodedados/test.sql
@@ -0,0 +1,20 @@
+/**
+comando para buscar dados:
+SELECT * FROM persons_laravel WHERE name LIKE value% or pet LIKE value% or age LIKE value%
+*/
+
+/**
+comando para apagar dados:
+DELETE FROM persons_laravel WHERE id = value;
+*/
+
+/**
+id se autoincrementa
+comando para inserir dados:
+SELECT * FROM persons_laravel(name,age,pet) VALUES(value,value,value)
+*/
+
+/*
+comando para atualizar dados:
+UPDATE persons_laravel(name,age,pet) WHERE id = value
+*/
